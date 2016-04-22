92c9b7488ed8c60ae68c0340ff6d728231f3a19f
vkeygarg <v.garg2@gmail.com>
2016-04-21 20:38:05 -0700

    Working code for new Declarations

--- AgileUpdateTableAttribute/src/main/java/com/x/agile/px/bo/UpdateTableBO.java
+++ AgileUpdateTableAttribute/src/main/java/com/x/agile/px/bo/UpdateTableBO.java
@@ -3,11 +3,16 @@ package com.x.agile.px.bo;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 import org.apache.log4j.Logger;
 
 import com.agile.api.APIException;
+import com.agile.api.DeclarationConstants;
+import com.agile.api.IAgileClass;
+import com.agile.api.IAttribute;
 import com.agile.api.ICell;
 import com.agile.api.IDataObject;
 import com.agile.api.IRow;
@@ -53,34 +58,70 @@ public class UpdateTableBO {
 	 */
 	public String updateTableAttr(IDataObject dataObj) throws APIException {
 		String msg = "";
-		String attrBaseID = prop.getProperty("FILLDOWN_ATTR_BASE_ID");
+		String attrExemBaseID = prop.getProperty("DECLARATION_MFR_ATTR_EXEMPTION_BASE_ID");
+		String attrDecCompBaseID = prop.getProperty("DECLARATION_MFR_ATTR_DEC_COMP_BASE_ID");
+		// Object [] attvalArr = new Object[attrBaseIDArr.length];
 		try {
 			ITable updTable = dataObj.getTable(Integer.parseInt(prop.getProperty("FILLDOWN_TABLE_BASE_ID")));
+			// IAgileClass cls = dataObj.getAgileClass();
+			// IAttribute attr =
+			// cls.getAttribute(Integer.parseInt(prop.getProperty("TAB_SORT_ORDER_ATTR_ID")));
+			// Specify the sort attribute for the table iterator
+			// ITable.ISortBy sortByNumber = updTable.createSortBy(attr,
+			// ITable.ISortBy.Order.ASCENDING);
+			// Create a sorted table iterator
+			// ITwoWayIterator updItr = updTable.getTableIterator(new
+			// ITable.ISortBy[] {sortByNumber});
+
 			ITwoWayIterator updItr = updTable.getTableIterator();
 			IRow updTabRow = null;
-			String attrVal = "";
+			Object attrDecCompVal = null;
+			Object attrExempVal = null;
+
 			while (updItr.hasNext()) {
 				updTabRow = (IRow) updItr.next();
-				attrVal = updTabRow.getValue(Integer.parseInt(attrBaseID)).toString();
-				if (!attrVal.isEmpty()) {
+				attrDecCompVal = updTabRow.getValue(Integer.parseInt(attrDecCompBaseID));
+				attrExempVal = updTabRow.getValue(Integer.parseInt(attrExemBaseID));
+				if ((attrDecCompVal != null && !attrDecCompVal.toString().isEmpty())
+						|| (attrExempVal != null && !attrExempVal.toString().isEmpty())) {
 					break;
 				}
 			}
-			if (!attrVal.isEmpty()) {
-				ICell cellObj = null;
+
+			if ((attrDecCompVal != null && !attrDecCompVal.toString().isEmpty())
+					|| (attrExempVal != null && !attrExempVal.toString().isEmpty())) {
+				// ICell cellObjDecComp = null;
+				// ICell cellObjExem = null;
 				updItr = updTable.getTableIterator();
 				updTabRow = null;
+				Map valMap = null;
 				while (updItr.hasNext()) {
 					updTabRow = (IRow) updItr.next();
-					cellObj = updTabRow.getCell(Integer.parseInt(attrBaseID));
-					if (cellObj!=null) {
-						cellObj.setValue(attrVal);
-					}
+					valMap = new HashMap();
+					if (attrExempVal != null)
+						valMap.put(Integer.parseInt(attrExemBaseID), attrExempVal);
+					if (attrDecCompVal != null)
+						valMap.put(Integer.parseInt(attrDecCompBaseID), attrDecCompVal);
+					// cellObjExem =
+					// updTabRow.getCell(Integer.parseInt(attrExemBaseID));
+					// updTabRow.setValue(Integer.parseInt(attrExemBaseID),attrExempVal);
+					// updTabRow.setValue(Integer.parseInt(attrDecCompBaseID),attrDecCompVal);
+					updTabRow.setValues(valMap);
+					/*
+					 * if (cellObjExem!=null) {
+					 * cellObjExem.setValue(attrExempVal); }
+					 */
+					// cellObjDecComp =
+					// updTabRow.getCell(Integer.parseInt(attrDecCompBaseID));
+					/*
+					 * if (cellObjDecComp!=null) {
+					 * cellObjDecComp.setValue(attrDecCompVal); }
+					 */
 				}
-			}else{
+			} else {
 				msg = prop.getProperty("ATTR_VALUE_EMPTY_MESSAGE");
 			}
-			
+
 		} catch (NumberFormatException e) {
 			logger.error(e.getMessage(), e);
 			msg = e.getMessage();
